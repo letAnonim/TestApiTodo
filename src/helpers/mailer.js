33bd901d194cc73b43af71ee646265a8cca1ff6d
Taras <65020628+letAnonim@users.noreply.github.com>
2021-08-28 15:34:02 +0300

    Update mailer.js
    Best practices to use await besides of callback functions

--- src/helpers/mailer.js
+++ src/helpers/mailer.js
@@ -14,11 +14,18 @@ const transporter = nodemailer.createTransport(
     from: "Test api <popovskyi.python@gmail.com>"
   });
 
-const mailer = (message) => {
-  transporter.sendMail(message, (err, info) => {
-    if (err) return console.log(err);
-    console.log(info);
-  })
+const mailer = async(message) => {
+  try {
+    const res = await transporter.sendMail(message)
+
+    // remove this line in PROD
+    console.log(res)
+
+  } catch (err) {
+    console.error(err);
+    throw new Error('err in mailer')
+  }
 }
 
+//use export default
 module.exports = mailer;
